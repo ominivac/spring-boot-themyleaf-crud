40ff4515bcbf6a17a76dc5090600a44189791e55
Equipe14 <equipe14@inova.serpro.gov.br>
2021-10-16 16:35:41 -0300

    1 commmit - confuguracao

---
+++ src/main/java/com/javaguides/SpringBootThemyleafCrudApplication.java
@@ -0,0 +1,13 @@
+package com.javaguides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThemyleafCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThemyleafCrudApplication.class, args);
+	}
+
+}
